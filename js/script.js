7ac41909aa295820e17ba72ff5f7c7bf623bcc89
abdeali888 <abdealiarsiwala888@gmail.com>
2022-11-30 23:57:18 +0530

    timer

--- js/script.js
+++ js/script.js
@@ -10,7 +10,7 @@ $(document).on('click', function(){
 });
 
 // Set the date we're counting down to
-var countDownDate = new Date("Jan 08, 2022 19:30:00").getTime();
+var countDownDate = new Date("Jan 08, 2023 19:30:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
